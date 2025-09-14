b8374b841cc3af52e6083215989fb04e25709b48
murasemaru <bp21071@shibaura-it.ac.jp>
2025-09-14 18:12:31 +0900

    conflict resolved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,20 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
 
   def show
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
+      reset_session
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user  # ユーザのプロフィール画面にリダイレクト
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
@@ -19,7 +22,8 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:name,:email,:password,:password_confirmation)
-  end
+    def user_params
+        params.require(:user).permit(:name, :email, :password,
+                                    :password_confirmation)
+    end
 end
